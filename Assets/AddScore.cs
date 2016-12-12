851b9753aee4493c844d7a75fcabf7b7bb787237
AD\u1002582 <yangyuepku@hotmail.com>
2016-12-11 18:59:54 -0700

    features: 1. awsd move, right click drag to rotate facing; 2. double jump; 3. press q to float in the air, and move mouse to rotate gravity direction; press q again to bring gravity back; 4. snap to the ground while the angle between ground surface normal direction and gravity direction >35 degrees; 5. double-sided shaders; 6. change the object which blocks my eyesight into transparent, and revert it back it no longer blocks me.

---
+++ Assets/AddScore.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddScore : MonoBehaviour {
+
+	public int ScoreToWin = 10;
+	private int CurrentScore = 0;
+
+	void GetScore() 
+	{
+		CurrentScore++;
+		Debug.Log ("Score: "+ CurrentScore);
+		if (CurrentScore >= ScoreToWin) 
+		{
+			Debug.Log ("Win!");
+		}
+	}
+		
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Star")
+		{
+			Destroy (collision.gameObject);
+			GetScore();
+		}	
+	}
+
+}
